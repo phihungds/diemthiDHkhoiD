fbeeb285c8d7e7a2e855a9ec770a1b2cf0f3fea9
Phi Hung <phihunds1@gmail.com>
2020-10-23 20:49:23 +0700

    them switch case

--- diem.js
+++ diem.js
@@ -2,5 +2,12 @@ function tinhdiem() {
     let diemToan = Number(document.getElementById("toan").value)
     let diemVan = Number(document.getElementById("van").value)
     let diemAnh = Number(document.getElementById("anh").value)
-    
+    let tongDiem = diemToan + diemVan + diemAnh
+    let khuVuc = document.getElementById("khuvuc").value
+    switch (khuVuc) {
+        case KV1:
+            document.getElementById("hienthi").innerText = "Điểm thi ĐH khối D của bạn là " + tongDiem + 0.75
+        break
+        case KV2:
+        }
 }
