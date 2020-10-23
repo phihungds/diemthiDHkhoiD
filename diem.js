08ac8289608016bfae5877484edc1ab3ffabab28
Phi Hung <phihunds1@gmail.com>
2020-10-23 21:25:44 +0700

    hoanthien

--- diem.js
+++ diem.js
@@ -2,12 +2,22 @@ function tinhdiem() {
     let diemToan = Number(document.getElementById("toan").value)
     let diemVan = Number(document.getElementById("van").value)
     let diemAnh = Number(document.getElementById("anh").value)
-    let tongDiem = diemToan + diemVan + diemAnh
     let khuVuc = document.getElementById("khuvuc").value
     switch (khuVuc) {
-        case KV1:
-            document.getElementById("hienthi").innerText = "Điểm thi ĐH khối D của bạn là " + tongDiem + 0.75
-        break
-        case KV2:
-        }
+        case "KV1":
+            let tongDiemkv1 = diemToan + diemVan + diemAnh + 0.75
+            document.getElementById("hienthi").innerText = "Điểm thi ĐH của bạn là " + tongDiemkv1
+            break
+        case "KV2":
+            let tongDiemkv2 = diemToan + diemVan + diemAnh + 0.25
+            document.getElementById("hienthi").innerText = "Điểm thi ĐH của bạn là " + tongDiemkv2
+            break
+        case "KV2-NT":
+            let tongDiemkv2nt = diemToan + diemVan + diemAnh + 0.5
+            document.getElementById("hienthi").innerText = "Điểm thi ĐH của bạn là " + tongDiemkv2nt
+            break
+        case "KV3":
+            let tongDiem = diemToan + diemVan + diemAnh
+            document.getElementById("hienthi").innerText = "Điểm thi ĐH của bạn là " + tongDiem
+    }
 }
