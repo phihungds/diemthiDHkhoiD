c50b34b6b9313dd0f31a40794b1ec6e7c45f973d
Phi Hung <phihunds1@gmail.com>
2020-10-23 20:36:49 +0700

    dat bien

---
+++ diem.js
@@ -0,0 +1,6 @@
+function tinhdiem() {
+    let diemToan = Number(document.getElementById("toan").value)
+    let diemVan = Number(document.getElementById("van").value)
+    let diemAnh = Number(document.getElementById("anh").value)
+    
+}
